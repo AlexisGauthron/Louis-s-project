e36069d659105cbde329090fa0741a7a400f0831
Alexis Gauthron <alexis.gauthron@edu.ece.fr>
2023-12-28 16:08:50 +0100

    algo base

--- main.c
+++ main.c
@@ -1,3 +1,14 @@
 //
 // Created by Alexis Gauthron Marq on 28/12/2023.
 //
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    printf("Are you ready\n");
+    for(int i=0;i<50;i++){
+        printf("%d\n",i);
+    }
+    return 0;
+}
